95498d515306d64b5a3cbd6e26fe1d846e2d15fd
Actatus <itsactatus@yahoo.com>
2022-09-29 14:01:26 -0700

    Add variables for input, submit, output containers

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+    
+    const apiKey = config.APIKey; //pulls API key from seperate file.
+
+    const searchInput = document.getElementById('search-input')
+    const searchButton = document.getElementById('search-button');
+    const searchOutputContainer = document.getElementById('search-output-container');
+
+});
