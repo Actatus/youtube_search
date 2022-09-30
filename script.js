0990abee1389c56fde225c6e6af8ed0f578878a7
Actatus <itsactatus@yahoo.com>
2022-09-30 03:47:37 -0700

    Begin fetch request, returns data

--- script.js
+++ script.js
@@ -9,9 +9,29 @@ document.addEventListener('DOMContentLoaded', function(){
     searchButton.addEventListener('click', () => {
         let searchQuery = searchInput.value;
 
+        //Check that there is input before performing the search
         if (!searchQuery){
             alert('Invalid search. Is the search bar empty?');
         }
-    })
+
+        retrieveDataFromAPI(searchQuery);
+    });
+
+    function retrieveDataFromAPI(query){
+        let requestOptions = {
+            method: "GET",
+            redirect: "follow"
+        };
+
+        return fetch("https://www.googleapis.com/youtube/v3/channels?forUsername="+ query + '&key=' + apiKey, requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                console.log(result);
+            })
+            .catch(error => {
+                console.log('error in fetch');
+                console.log(error);
+            });
+    }
 
 });
